4f2d7fc6c360d17532b00d03932ffdaac8560d6d
Nandhini08502312 <2018nandhinidevi@gmail.com>
2022-08-23 22:33:00 +0530

    flutter packages

---
+++ lib/easy_onboard.dart
@@ -0,0 +1,33 @@
+import 'package:easy_onboard/easy_onboard.dart';
+
+import 'package:flutter/material.dart';
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Onboard(
+        primaryColor: const Color(0xff6C63FF),
+        onboardPages: onboardPages,
+        skipButtonPressed: () {
+          // Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondPage()));
+        },
+        skipText: 'Skip',
+        nextText: 'Next',
+        lastText: 'Start');
+  }
+
+  List<OnboardModel> onboardPages = [
+    OnboardModel(imagePath: 'assets/images/barbecu.png', title: 'Order Your Food', subTitle: 'Now you can order food any time right from your phone.'),
+    OnboardModel(imagePath: 'assets/images/hamburger.png', title: 'Cooking Safe Food', subTitle: 'We are maintain safty and We keep clean while making food.'),
+    OnboardModel(imagePath: 'assets/images/ice_cream.png', title: 'Quick Delivery', subTitle: 'Orders your favourite meals will be immediately deliver.'),
+  ];
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+import 'package:video_player/gradient.dart';
+
+import 'dropdownsearch.dart';
+import 'easy_onboard.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage1(),
+    );
+  }
+}
